02ae1e8a3c51fb95d54c6f6583ca98ac25c75663
Louis Brutton <louisbrutton@gmail.com>
2015-01-16 09:36:45 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rickrockstar::Application.config.secret_token = '19a87e2722ea9d69b42008eacebf1ee7bd2e841a8ed3fd04cde3dc65e94a4a2104e9aa09ed39f616b3644851cece9d36cb50d08899b53fbb124b93a1ad17867f'
